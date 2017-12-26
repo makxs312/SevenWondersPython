3b785713e48d08f738e3604a54a4ffe8630c7016
Maks <yamax0312@gmail.com>
2017-12-26 09:40:02 +0200

    Added unit tests

---
+++ 7WondersPython/tests.py
@@ -0,0 +1,52 @@
+import unittest
+import json
+from runserver import app
+from werkzeug.datastructures import MultiDict, ImmutableMultiDict
+
+class SevenWonders_tests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass 
+
+    @classmethod
+    def tearDownClass(cls):
+        pass 
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True 
+
+    def tearDown(self):
+        pass 
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/') 
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200) 
+
+    def test_home_data(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/') 
+
+        # assert the response data
+        self.assertEqual(result.status_code, 200)
+
+    def test_get_cities(self):
+        result = self.app.get('/api/Cities/GetCities')
+        self.assertEqual(result.status_code, 200)
+
+    def test_get_city(self):
+        result = self.app.get('/api/Cities/GetCity', query_string={'id':'29'}, content_type='application/json',
+            environ_base={
+                'HTTP_USER_AGENT': 'Chrome',
+                'REMOTE_ADDR': '127.0.0.1'
+            })
+        self.assertEqual(result.status_code, 200)
+if __name__ == '__main__':
+    unittest.main()
